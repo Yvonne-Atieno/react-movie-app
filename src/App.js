2c0ddc4638663e6c5c3a121adf99aec77f71944c
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-07-31 07:38:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import GetMovies from './Components/getmovies';
+import './App.css';
+import Navbar from './Components/Navbar';
+// import MovieLists from './Components/carousel';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <GetMovies/>
+    </div>
+  );
+}
+
+export default App;
